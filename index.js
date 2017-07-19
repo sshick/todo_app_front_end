ac78ebfd42d782c07ad8ca51ce88baed46466dcd
Sophia Shick <sophie.shick@comcast.net>
2017-07-19 17:52:17 +0000

    attempted to add index js commands, partially failed

---
+++ index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#submit-button").click(function(){
+        var todoBlock = $(this).closest("#new-todo-block")
+        var description = todoBlock.find("#enter-task")
+    $("ul").append("<li> <input type='checkbox'/>" + description.val() + "<span class='todo'>" + "</span> </li>")
+    description.val("")
+    })
+})
