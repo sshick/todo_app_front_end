2d7fe9db142901beed5381114a3c11bcad8b609c
Sophia Shick <sophie.shick@comcast.net>
2017-07-19 20:05:13 +0000

    fixed the index page bugs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("#submit-button").click(function(){
-        var todoBlock = $(this).closest("#new-todo-block")
-        var description = todoBlock.find("#enter-task")
+        var todoBlock = $(this).closest("#new-todo-block");
+        var description = todoBlock.find("#enter-task");
     $("ul").append("<li> <input type='checkbox'/>" + description.val() + "<span class='todo'>" + "</span> </li>")
     description.val("")
     })
